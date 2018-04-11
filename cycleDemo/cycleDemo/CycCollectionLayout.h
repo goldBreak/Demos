3409aaf3ec14fdfe4fa27c5e1b99c623f4a864a1
GaoF <1027632706@qq.com>
2018-04-11 14:56:39 +0800

    Add files via upload

---
+++ cycleDemo/cycleDemo/CycCollectionLayout.h
@@ -0,0 +1,13 @@
+//
+//  CycCollectionLayout.h
+//  cycleDemo
+//
+//  Created by xsd on 2018/4/10.
+//  Copyright © 2018年 com.GF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CycCollectionLayout : UICollectionViewFlowLayout
+
+@end
---
+++ cycleDemo/cycleDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  cycleDemo
+//
+//  Created by xsd on 2018/4/10.
+//  Copyright © 2018年 com.GF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
